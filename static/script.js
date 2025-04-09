e1972d269b3b18f671f40e05dbebc8b49ebe20d9
Lucas Galindo <devlucasgalindo@gmail.com>
2025-04-09 16:12:22 -0300

    atualização das animações utilizando de JS para fazer as animações junto do CSS

--- static/script.js
+++ static/script.js
@@ -19,3 +19,22 @@ navItem.forEach(item => {
         }
     });
 });
+
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((Element) =>{
+        if (windowTop > Element.offsetTop){
+            Element.classList.add("animate");
+        } else {
+            Element.classList.remove("animate");
+        };
+    });
+};
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+});
