2d0c304386a3a210daef8d529190a94dcfc41ba6
Lucas Galindo <devlucasgalindo@gmail.com>
2025-04-11 08:59:42 -0300

    atualização de melhorias, fiz a troca do flask e optei por utilizar FastAPI, tambem estou usando da  API do EmailJS para efetuar o envio do email de forma gratuita e simples

--- static/script.js
+++ static/script.js
@@ -38,3 +38,11 @@ const animeScroll = () => {
 window.addEventListener("scroll", ()=>{
     animeScroll();
 });
+
+const btnEnviar = document.querySelector('#btn-enviar');
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader');
+
+btnEnviar.addEventListener("click", ()=>{
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none";
+})
